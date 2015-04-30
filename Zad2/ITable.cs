a68d99f265e3e78abc8d45504730c95ba3f858f3
Adam Czyżyński <adam.czyzynski@hotmail.com>
2015-04-30 12:21:36 +0200

    indexator

---
+++ Zad2/ITable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zad2
+{
+    interface ITable
+    {
+
+        public int getElement(int index);
+        public int addElement(int element);
+
+    
+
+    }
+}
---
+++ Zad2/Table.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zad2
+{
+    class Table{
+
+    
+   private int[] temps;
+
+
+    public int Length
+    {
+        get { return temps.Length; }
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            return temps[index];
+        }
+
+        set
+        {
+            temps[index] = value;
+        }
+    }
+
+
+
+}
